fc67fb260ce1757d5191108594014ace5fcc154e
Roman Weich <roman.weich@gmail.com>
2021-05-27 16:52:45 +0200

    feat: change settings dialog
    - add primeflex css lib and make the settingsdialog look a little bit better
    - add settings-storage and handling of loading/saving/initializing

--- src/Utils/Settings/Settings.ts
+++ src/Utils/Settings/Settings.ts
@@ -1,31 +1,39 @@
 import 'primevue/resources/primevue.min.css';
 import 'primevue/resources/themes/saga-blue/theme.css';
+import 'primeflex/primeflex.css';
 
 import * as PrimeVue from 'primevue/config';
 
+import DefaultSettings from '@/Utils/Settings/DefaultSettings';
 import { EventEmitter } from 'eventemitter3';
 import { Logger } from 'ts-log';
 import SettingsApp from '@/Utils/Settings/SettingsApp.vue';
 import { SettingsType } from '@/Utils/Settings/SettingsType';
+import Storage from '@/Utils/Storage';
 import Vue from 'vue';
 import VueLogger from '@/Utils/Settings/VueLogger';
 
 type EventType = {
-  saveSettings: (settings: SettingsType) => void;
+  newSettings: (settings: SettingsType) => void;
 };
+export type SettingsStorageType = Storage<{ settings: SettingsType }>;
 
 export default class Settings {
+  private readonly storage: SettingsStorageType;
   private readonly logger: Logger;
   private readonly eventEmitter = new EventEmitter<EventType>();
 
-  constructor(logger: Logger) {
+  private readonly SETTINGS_SAVE_TIME_MS = 365 * 24 * 60 * 60 * 1000;
+
+  constructor(storage: SettingsStorageType, logger: Logger) {
+    this.storage = storage;
     this.logger = logger;
   }
 
   /**
    * Creates and registers the settings dialog to the userscript menu
    */
-  public register(): this {
+  public registerDialog(): this {
     const div = document.createElement('div');
     div.id = 'userscript-settings-container';
     const body = document.querySelector('body');
@@ -38,21 +46,33 @@ export default class Settings {
     const vue = new Vue({
       el: div,
       render: (h) => h(SettingsApp),
-    }).$on('saveSettings', (settings: SettingsType) => this.eventEmitter.emit('saveSettings', settings));
-
-    GM.registerMenuCommand('Settings ...', () => {
-      this.logger.info('opening settings dialog ...');
-      vue.$children[0].$data.showDialog = true;
+    }).$on('saveSettings', (settings: SettingsType) => {
+      this.eventEmitter.emit('newSettings', settings);
+      this.storeSettings(settings);
     });
+
+    GM.registerMenuCommand('Settings ...', () => this.openSettingsDialog(vue));
+    this.eventEmitter.emit('newSettings', DefaultSettings.merge(this.storage.get('settings')));
 
     return this;
   }
 
   /**
    * Allows to register a callback function no be notified when the settings were changed
-   * @param callback
    */
-  public onSave(callback: (settings: SettingsType) => void): void {
-    this.eventEmitter.on('saveSettings', callback);
+  public onNewSettings(callback: EventType['newSettings']): this {
+    this.eventEmitter.on('newSettings', callback);
+    return this;
+  }
+
+  private storeSettings(settings: SettingsType): void {
+    this.logger.info('saving settings to storage', settings);
+    this.storage.set('settings', settings, this.SETTINGS_SAVE_TIME_MS);
+  }
+
+  private openSettingsDialog(vue: Vue): void {
+    this.logger.info('opening settings dialog ...');
+    vue.$children[0].$data.showDialog = true;
+    vue.$children[0].$data.injectSettings(this.storage.get('settings'));
   }
 }
--- src/Utils/Settings/SettingsApp.vue
+++ src/Utils/Settings/SettingsApp.vue
@@ -1,12 +1,14 @@
 <template>
   <div class="settingsapp">
-    <Dialog header="Settings" :visible.sync="showDialog" :modal="true">
-      <div class="p-formgroup-inline">
-        <div class="p-field">
-          <h3>Enable:</h3>
-          <InputSwitch v-model="enabled" />
+    <Dialog header="Settings" :visible.sync="showDialog" :modal="true" :style="{ width: '25vw' }">
+      <Fieldset legend="Misc" class="p-mt-5">
+        <div class="p-grid p-align-center p-mt-2 p-mx-2">
+          <div class="p-col p-grid p-align-center">
+            <label for="enabled" class="p-mr-2">Enable Userscript:</label>
+            <InputSwitch id="enabled" v-model="enabled" />
+          </div>
         </div>
-      </div>
+      </Fieldset>
 
       <template #footer>
         <Button label="Cancel" icon="pi pi-times" class="p-button-text" @click="showDialog = false" />
@@ -19,13 +21,17 @@
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 import Button from 'primevue/button/Button';
+import DefaultSettings from "@/Utils/Settings/DefaultSettings";
 import Dialog from 'primevue/dialog/Dialog';
+import Fieldset from 'primevue/fieldset/Fieldset';
 import InputSwitch from 'primevue/inputswitch/InputSwitch';
+import {SettingsType} from "@/Utils/Settings/SettingsType";
 
 @Component({
   components: {
     Button,
     Dialog,
+    Fieldset,
     InputSwitch
   }
 })
@@ -35,12 +41,22 @@ export default class SettingsApp extends Vue {
   // settings data
   enabled = true;
 
+  // prop for easy external settings injection
+  injectSettings = (settings?: Partial<SettingsType>): void => this.loadSettings(settings);
+
   public save(): void {
     this.$logger.info('saving settings ...');
     this.showDialog = false;
-    this.$root.$emit('saveSettings', {
+    const settings: SettingsType = {
       enabled: this.enabled
-    });
+    };
+    this.$root.$emit('saveSettings', settings);
+  }
+
+  private loadSettings(payload?: Partial<SettingsType>): void {
+    const settings = DefaultSettings.merge(payload);
+    this.$logger.info('loading settings ...', settings);
+    this.enabled = settings.enabled;
   }
 }
 </script>
